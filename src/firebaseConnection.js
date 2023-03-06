13fe813663c26349a87931f44b9fe598baa352c5
mateus <mateuscdiniz@gmail.com>
2023-03-06 18:44:09 -0300

    adds dotenv

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC2Ls2WVYPc-_pn0KlroBEy905T1BKW6mk',
-  authDomain: 'inxepa-form.firebaseapp.com',
-  projectId: 'inxepa-form',
-  storageBucket: 'inxepa-form.appspot.com',
-  messagingSenderId: '607140645618',
-  appId: '1:607140645618:web:e869b9382cb6bb5d9d5e6b',
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
